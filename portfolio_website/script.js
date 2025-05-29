d67c45ac2a04cb9cddcfdb5e62ee743e13d70e8f
raghav-di <raghavdhyaniwork15.com>
2025-05-29 17:01:46 +0530

    5th commit

--- portfolio_website/script.js
+++ portfolio_website/script.js
@@ -1,8 +1,7 @@
-// Sticky Navigation Menu
 let nav = document.querySelector("nav");
 let scrollBtn = document.querySelector(".scroll-button a");
 
-// Show/hide sticky navigation and scroll button based on scroll position
+
 window.onscroll = function () {
   if (document.documentElement.scrollTop > 20) {
     nav.classList.add("sticky");
@@ -13,13 +12,11 @@ window.onscroll = function () {
   }
 };
 
-// Side Navigation Menu
 let body = document.querySelector("body");
 let navBar = document.querySelector(".navbar");
 let menuBtn = document.querySelector(".menu-btn");
 let cancelBtn = document.querySelector(".cancel-btn");
 
-// Open side navigation
 menuBtn.onclick = function () {
   navBar.classList.add("active");
   menuBtn.style.opacity = "0";
@@ -36,10 +33,8 @@ const hideNavMenu = () => {
   scrollBtn.style.pointerEvents = "auto";
 };
 
-// Close side navigation
 cancelBtn.onclick = hideNavMenu;
 
-// Close side navigation when a menu link is clicked
 let navLinks = document.querySelectorAll(".menu li a");
 navLinks.forEach((link) => {
   link.addEventListener("click", hideNavMenu);
